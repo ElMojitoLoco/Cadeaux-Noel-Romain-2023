f212f5ce717772c3773c0a9bab85b84209de57b7
ElMojitoLoco <124294816+ElMojitoLoco@users.noreply.github.com>
2023-12-07 18:35:15 +0100

    ajout fichiers site web

---
+++ script.js
@@ -0,0 +1,76 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var gifts = [
+        {
+            name: "Livre Audio : Murtagh",
+            image: "./ressources/audiobook.jpg",
+            price: "27€",
+            url: "https://books.apple.com/fr/audiobook/murtagh/id1707560614"
+        },
+        {
+            name: "Abonnement Midjourney",
+            image: "./ressources/midjourney.jpg",
+            price: "28€",
+            url: "https://docs.midjourney.com/docs/plans"
+        },
+        {
+            name: "Cours Udemy",
+            image: "./ressources/cours_go.jpg",
+            price: "20€",
+            url: "https://www.udemy.com/course/go-programming-golang-the-complete-developers-guide/"
+        },
+        {
+            name: "Autre cours Udemy",
+            image: "./ressources/cours_typescript.jpg",
+            price: "30€",
+            url: "https://www.udemy.com/course/typescript-the-complete-developers-guide/"
+        },
+        {
+            name: "Lampe Spacebuddy Pleshy",
+            image: "./ressources/space_buddy.jpg",
+            price: "56€",
+            url: "https://pleshy.fr/products/spacebuddy-3"
+        }
+    ];
+
+    var giftList = document.getElementById('gift-list');
+
+    gifts.forEach(function (gift) {
+        var giftElement = document.createElement('div');
+        giftElement.classList.add('gift');
+
+        var giftLink = document.createElement('a');
+        giftLink.href = gift.url;
+        giftLink.target = "_blank";
+
+        var giftImage = document.createElement('img');
+        giftImage.src = gift.image;
+        giftImage.alt = gift.name;
+        giftLink.appendChild(giftImage);
+
+        giftElement.appendChild(giftLink);
+
+        var giftName = document.createElement('div');
+        giftName.textContent = gift.name;
+        giftElement.appendChild(giftName);
+
+        var giftPrice = document.createElement('div');
+        giftPrice.textContent = gift.price;
+        giftElement.appendChild(giftPrice);
+
+        giftElement.addEventListener('click', function () {
+            selectGift(gift);
+        });
+
+        giftList.appendChild(giftElement);
+    });
+
+    function selectGift(selectedGift) {
+        gifts.forEach(function (gift, i) {
+            if (gift !== selectedGift) {
+                var giftElements = document.getElementsByClassName('gift');
+                giftElements[i].style.display = 'none';
+            }
+        });
+        alert("Merci d'avoir choisi : " + selectedGift.name);
+    }
+});
